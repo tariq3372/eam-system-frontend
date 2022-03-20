86ca5e815247b872ae53fa31617c255ba85f42b2
Your Name <tariqfaqira@gmail.com>
2022-03-20 16:06:26 +0300

    view component is added for employee, department and jobTitles

---
+++ src/components/Employees/ViewEmployeeModal.jsx
@@ -0,0 +1,80 @@
+import { Dialog, DialogContent, Divider, MenuItem, Stack, TextField, Typography } from '@mui/material'
+import React from 'react'
+import InputWrapper from '../InputWrapper'
+
+const ViewEmployeeModal = ({ onClose, item }) => {
+    return (
+        <Dialog
+            onClose={onClose}
+            open={true}
+            fullWidth={true}
+            maxWidth={"sm"}
+        >
+            <DialogContent>
+                <Stack direction='column' spacing={3}>
+                    <Typography sx={{ textAlign: 'center', fontWeight: 'bold', fontSize: 20 }}>View Employee</Typography>
+                    <Divider sx={{ mb: 3 }} />
+                    <Stack direction='column' spacing={2}>
+                        <InputWrapper>
+                            <TextField
+                                label="First Name"
+                                value={item?.fname}
+                                disabled
+                            />
+                        </InputWrapper>
+
+                        <InputWrapper>
+                            <TextField
+                                label="Last Name"
+                                value={item?.lname}
+                                disabled
+                            />
+                        </InputWrapper>
+
+                        <InputWrapper>
+                            <TextField
+                                label="Gender"
+                                value={item?.gender}
+                                disabled
+                            >
+                                <MenuItem key={"MALE"} value={"MALE"}>
+                                    MALE
+                                </MenuItem>
+                                <MenuItem key={"FEMALE"} value={"FEMALE"}>
+                                    FEMALE
+                                </MenuItem>
+                            </TextField>
+                        </InputWrapper>
+
+                        <InputWrapper>
+                            <TextField
+                                label="Age"
+                                value={item?.age}
+                                disabled
+                            />
+                        </InputWrapper>
+
+                        <InputWrapper>
+                            <TextField
+                                label="Address"
+                                value={item?.contactAdd}
+                                disabled
+                            />
+                        </InputWrapper>
+
+                        <InputWrapper>
+                            <TextField
+                                label="Email"
+                                value={item?.email}
+                                disabled
+                            />
+                        </InputWrapper>
+
+                    </Stack>
+                </Stack>
+            </DialogContent>
+        </Dialog>
+    )
+}
+
+export default ViewEmployeeModal
---
+++ src/components/JobTitles/ViewJobTitlesModal.jsx
@@ -0,0 +1,32 @@
+import { Dialog, DialogContent, Divider, MenuItem, Stack, TextField, Typography } from '@mui/material'
+import React from 'react'
+import InputWrapper from '../InputWrapper'
+
+const ViewJobTitlesModal = ({ onClose, item }) => {
+    return (
+        <Dialog
+            onClose={onClose}
+            open={true}
+            fullWidth={true}
+            maxWidth={"sm"}
+        >
+            <DialogContent>
+                <Stack direction='column' spacing={3}>
+                    <Typography sx={{ textAlign: 'center', fontWeight: 'bold', fontSize: 20 }}>View Employee</Typography>
+                    <Divider sx={{ mb: 3 }} />
+                    <Stack direction='column' spacing={2}>
+                        <InputWrapper>
+                            <TextField
+                                label="Job Title Name"
+                                value={item?.jobTitleName}
+                                disabled
+                            />
+                        </InputWrapper>
+                    </Stack>
+                </Stack>
+            </DialogContent>
+        </Dialog>
+    )
+}
+
+export default ViewJobTitlesModal
