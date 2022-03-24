239941968326de965e649d36566a8ec28415a279
Your Name <tariqfaqira@gmail.com>
2022-03-24 16:25:52 +0300

    leave request status is added in employee dashboard

--- src/pages/employee/Dashboard.jsx
+++ src/pages/employee/Dashboard.jsx
@@ -7,6 +7,7 @@ const Dashboard = () => {
   const [loading, setLoading] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
   const [isCheckIn, setIsCheckIn] = useState(false);
+  const [isLeave, setIsLeave] = useState(false);
   const id = localStorage.getItem('_id');
 
   useEffect(() => {
@@ -18,11 +19,14 @@ const Dashboard = () => {
     checkIsCheckInApi({ id }, (res) => {
       setLoading(false);
       if (res.data) {
+        if (res.data.isLeaveRequestExist) {
+          setIsLeave(true);
+        } else {
+          setIsLeave(false)
+        }
         if (res.data.isDurationExist) {
-          console.log(res.data);
           setIsCheckIn(false);
         } else {
-          console.log(res.data);
           setIsCheckIn(true);
         }
       };
@@ -75,12 +79,13 @@ const Dashboard = () => {
     setIsLoading(true);
     leaveRequestApi(id, (res) => {
       setIsLoading(false);
-      if(res.data) {
+      if (res.data) {
         Swal.fire(
           'Success',
           'Your request is send to admin',
           'success'
         )
+        handleCheckInStatue();
       } else {
         Swal.fire(
           'Error',
@@ -89,23 +94,6 @@ const Dashboard = () => {
         )
       }
     })
-    // leaveApi({ id }, (res) => {
-    //   setIsLoading(false);
-    //   if (res.data) {
-    //     Swal.fire(
-    //       'Success',
-    //       "Your request processed successfully",
-    //       "success"
-    //     );
-    //     handleCheckInStatue();
-    //   } else {
-    //     Swal.fire(
-    //       'Error',
-    //       "Something went wrong, Please try again later",
-    //       "error"
-    //     );
-    //   }
-    // })
   }
 
   if (loading || isLoading) return (
@@ -113,33 +101,37 @@ const Dashboard = () => {
       <CircularProgress color="inherit" />
     </Container>
   )
-
+  const renderLeaveRequest = () => {
+    if (!isLeave) {
+      return (<Card style={{ backgroundColor: "lightsalmon" }}>
+        <CardContent sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+          <Typography>{"Want leave"}</Typography>
+          <Button variant="contained" size="large" onClick={() => handleLeave()} >Leave Request</Button>
+        </CardContent>
+      </Card>)
+    }
+  }
   return (
     <Container style={{ minHeight: "100%", display: "flex" }}>
       <Stack flex={1} direction="column" spacing={5}>
-        {!isCheckIn ?
-          <>
+        <>
+          {!isCheckIn ?
             <Card style={{ backgroundColor: "lightgreen" }}>
               <CardContent sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
                 <Typography>{"Want to check in"}</Typography>
                 <Button variant="contained" size="large" onClick={() => handleCheckIn()} >Check In</Button>
               </CardContent>
             </Card>
-            <Card style={{ backgroundColor: "lightsalmon" }}>
+            :
+            <Card style={{ backgroundColor: "lightblue" }}>
               <CardContent sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-                <Typography>{"Want leave"}</Typography>
-                <Button variant="contained" size="large" onClick={() => handleLeave()} >Leave</Button>
+                <Typography>{"Want to check out"}</Typography>
+                <Button variant="contained" size="large" onClick={() => handleCheckOut()} >Checkout</Button>
               </CardContent>
             </Card>
-          </>
-          :
-          <Card style={{ backgroundColor: "lightblue" }}>
-            <CardContent sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-              <Typography>{"Want to check out"}</Typography>
-              <Button variant="contained" size="large" onClick={() => handleCheckOut()} >Checkout</Button>
-            </CardContent>
-          </Card>
-        }
+          }
+          {renderLeaveRequest()}
+        </>
       </Stack>
     </Container>
   );
