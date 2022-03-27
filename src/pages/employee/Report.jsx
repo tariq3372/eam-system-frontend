83ce3ee80a87d4861f97897245e22d5be9bc21f9
Your Name <tariqfaqira@gmail.com>
2022-03-27 09:38:44 +0300

    leave request status is added in report page in employee side

--- src/pages/employee/Report.jsx
+++ src/pages/employee/Report.jsx
@@ -6,17 +6,20 @@ const Report = () => {
   const [totalWorkingMinutes, settotalWorkingMinutes] = useState(0);
   const [totalLeaves, setTotalLeaves] = useState(0);
   const [totalSalary, setTotalSalary] = useState(0);
+  const [leaveRequestStatus, setLeaveRequestStatus] = useState("REJECTED");
   const [loading, setLoading] = useState(true);
   const _id = localStorage.getItem('_id');
 
   useEffect(() => {
     getEmployeeAttendanceReportsApi({ _id }, (res) => {
         if(res.data) {
+            console.log("data", res.data);
             setLoading(false);
-            const { totalWorkingMinutes, totalLeaves, totalSalary } = res.data
+            const { totalWorkingMinutes, totalLeaves, totalSalary, leaveRequestStatus } = res.data
             settotalWorkingMinutes(totalWorkingMinutes);
             setTotalLeaves(totalLeaves);
             setTotalSalary(totalSalary);
+            setLeaveRequestStatus(leaveRequestStatus);
         }
     })
   }, [])
@@ -32,21 +35,28 @@ const Report = () => {
         <Card style={{ backgroundColor: "lightgreen" }}>
           <CardContent>
             Total Working Minutes
-            <Typography>{renderCount(totalWorkingMinutes)}</Typography>
+            <Typography >{renderCount(totalWorkingMinutes)}</Typography>
           </CardContent>
         </Card>
 
         <Card style={{ backgroundColor: "lightblue" }}>
           <CardContent>
             Total Leaves
-            <Typography>{renderCount(totalLeaves)}</Typography>
+            <Typography >{renderCount(totalLeaves)}</Typography>
           </CardContent>
         </Card>
 
         <Card>
           <CardContent style={{ backgroundColor: "lightsalmon" }}>
             Total Salary
-            <Typography>{renderCount(totalSalary)}</Typography>
+            <Typography  >{renderCount(totalSalary)}</Typography>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardContent style={{ backgroundColor: "lightgray" }}>
+            Leave Request Status
+            <Typography>{renderCount(leaveRequestStatus)}</Typography>
           </CardContent>
         </Card>
 
